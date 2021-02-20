c21e8b8fdb9fb439f05d2c440dd9a53e2bb71ceb
koyo-miyamura <nexus9071@gmail.com>
2021-02-20 16:46:36 +0900

    capitalize_sentences

--- elixir/programming_elixir_2nd/StringsAndBinaries.exs
+++ elixir/programming_elixir_2nd/StringsAndBinaries.exs
@@ -2,4 +2,11 @@ defmodule MyString do
   def anagram?(str1, str2) do
     Enum.sort(str1) == Enum.sort(str2)
   end
+
+  def capitalize_sentences(str) do
+    str
+    |> String.split(". ")
+    |> Enum.map(&String.capitalize/1)
+    |> Enum.join(". ")
+  end
 end
