83fa59da273249abc1660af44e6e83813b1f15b3
koyo-miyamura <nexus9071@gmail.com>
2021-04-04 13:38:35 +0900

    Stackサーバ書いてみた

---
+++ elixir/programming_elixir_2nd/stack/lib/stack.ex
@@ -0,0 +1,16 @@
+defmodule Stack do
+  use GenServer
+
+  def init(stack) do
+    {:ok, stack}
+  end
+
+  def handle_call(:pop, _from, state) do
+    [value | new_state] = state
+    {:reply, value, new_state}
+  end
+
+  def handle_cast({:push, value}, state) do
+    {:noreply, [value | state]}
+  end
+end
