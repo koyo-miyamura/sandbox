4103ae278294f20fb53d711b4696a36ac08bb23c
koyo-miyamura <nexus9071@gmail.com>
2021-04-04 14:10:35 +0900

    外部API実装を書いた

--- elixir/programming_elixir_2nd/stack/lib/stack/server.ex
+++ elixir/programming_elixir_2nd/stack/lib/stack/server.ex
@@ -1,6 +1,18 @@
 defmodule Stack.Server do
   use GenServer
 
+  def start_link(init_stack) do
+    GenServer.start_link(__MODULE__, init_stack, name: __MODULE__)
+  end
+
+  def pop() do
+    GenServer.call(__MODULE__, :pop)
+  end
+
+  def push(value) do
+    GenServer.cast(__MODULE__, {:push, value})
+  end
+
   def init(stack) do
     {:ok, stack}
   end
