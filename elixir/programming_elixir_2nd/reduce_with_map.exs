02c65793c5fdfdbcf0e476c34e1c34975fd0ebee
koyo-miyamura <nexus9071@gmail.com>
2021-04-06 01:36:59 +0900

    mapを使ったreduceを書いてみた

---
+++ elixir/programming_elixir_2nd/reduce_with_map.exs
@@ -0,0 +1,15 @@
+defmodule M do
+  @doc """
+    Descriptions.
+
+  ## Examples
+    iex> M.do_add_words(["aa", "bb", "hoge"], %{})
+    %{2 => ["bb", "aa"], 4 => ["hoge"]}
+
+    iex> M.do_add_words(["aa", "bb", "hoge"], %{ 2 => ["cc"]})
+    %{2 => ["bb", "aa", "cc"], 4 => ["hoge"]}
+  """
+  def do_add_words(words, map), do: Enum.reduce(words, map, &do_add_word(&1,&2))
+
+  defp do_add_word(word, map), do: Map.update(map, String.length(word), [word], &[word|&1])
+end
