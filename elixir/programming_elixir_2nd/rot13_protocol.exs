3f14edb68cd38ad8e4f4fd553b56e8a10817eff8
koyo-miyamura <nexus9071@gmail.com>
2021-04-12 23:44:26 +0900

    プロトコルでシーザー暗号の実装

--- elixir/programming_elixir_2nd/rot13_protocol.exs
+++ elixir/programming_elixir_2nd/rot13_protocol.exs
@@ -2,3 +2,25 @@ defprotocol Caesar do
   def encrypt(string, shift)
   def rot13(string)
 end
+
+defimpl Caesar, for: BitString do
+  def encrypt(string, shift) do
+    string |> String.to_charlist |> Caesar.encrypt(shift) |> List.to_string
+  end
+  def rot13(string) do
+    encrypt(string, 13)
+  end
+end
+
+defimpl Caesar, for: List do
+  def encrypt(string, shift) do
+    Enum.map(string, &_encrypt(&1, shift)) |> Enum.join |> String.to_charlist
+  end
+  def rot13(string) do
+    encrypt(string, 13)
+  end
+
+  defp _encrypt(char, shift) do
+    [?a + rem(char + shift - ?a, 26)]
+  end
+end
