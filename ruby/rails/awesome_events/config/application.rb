3c60efdfa8a803de1fcf9b915bfb572231a4a1c4
koyo-miyamura <nexus9071@gmail.com>
2021-04-25 11:40:46 +0900

    eventモデル周りの作成

--- ruby/rails/awesome_events/config/application.rb
+++ ruby/rails/awesome_events/config/application.rb
@@ -21,15 +21,7 @@ Bundler.require(*Rails.groups)
 
 module AwesomeEvents
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'
   end
 end
