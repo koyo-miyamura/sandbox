b13d49ed98a1da7e6cba9032e57e5020c92467aa
koyo-miyamura <nexus9071@gmail.com>
2021-04-25 22:39:13 +0900

    showとindexの実装

--- ruby/rails/awesome_events/app/controllers/welcome_controller.rb
+++ ruby/rails/awesome_events/app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate
 
   def index
-    # do something
+    @events = Event.where('start_at > ?', Time.zone.now).order(:start_at)
   end
 end
