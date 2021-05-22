0064a587a08721bfcea3043698a181dfce6f4378
koyo-miyamura <nexus9071@gmail.com>
2021-05-22 23:42:13 +0900

    router.exs雛形の調整

--- elixir/router.exs
+++ elixir/router.exs
@@ -2,21 +2,21 @@ defmodule ShotrizeWeb.Router do
   use ShotrizeWeb, :router
 
   pipeline :browser do
-    plug(:accepts, ["html"])
-    plug(:fetch_session)
-    plug(:fetch_flash)
-    plug(:protect_from_forgery)
-    plug(:put_secure_browser_headers)
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
   end
 
   pipeline :api do
-    plug(:accepts, ["json"])
+    plug :accepts, ["json"]
   end
 
   scope "/", ShotrizeWeb do
-    pipe_through(:browser)
+    pipe_through :browser
 
-    get("/", PageController, :index)
+    get "/", PageController, :index
   end
 
   # Other scopes may use custom stacks.
@@ -35,8 +35,8 @@ defmodule ShotrizeWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/" do
-      pipe_through(:browser)
-      live_dashboard("/dashboard", metrics: ShotrizeWeb.Telemetry)
+      pipe_through :browser
+      live_dashboard "/dashboard", metrics: ShotrizeWeb.Telemetry
     end
   end
 end
