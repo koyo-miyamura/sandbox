e7dc8cfe8cc9e3a16da12b99a784caf89bc70e7a
koyo-miyamura <nexus9071@gmail.com>
2021-06-05 23:22:12 +0900

    コンポーネント化してみた

--- react/tailwind-next-sample/pages/index.js
+++ react/tailwind-next-sample/pages/index.js
@@ -1,6 +1,73 @@
 import Head from 'next/head'
 
 export default function Home() {
+  const LinkCard = (props) => (
+    <a
+      href={props.href}
+      className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+    >
+      <h3 className="text-2xl font-bold">{props.title}</h3>
+      <p className="mt-4 text-xl">
+        {props.text}
+      </p>
+    </a>
+  )
+
+  const linkCards = [
+    {
+      href: "https://nextjs.org/docs",
+      title: (
+        <>
+        Documentation &rarr;
+        </>
+      ),
+      text: (
+        <>
+        Find in-depth information about Next.js features and API.
+        </>
+      ),
+    },
+    {
+      href: "https://github.com/vercel/next.js/tree/master/examples",
+      title: (
+        <>
+        Examples &rarr;
+        </>
+      ),
+      text: (
+        <>
+        Discover and deploy boilerplate example Next.js projects.
+        </>
+      )
+    },
+    {
+      href: "https://nextjs.org/learn",
+      title: (
+        <>
+        Learn &rarr;
+        </>
+      ),
+      text: (
+        <>
+        Learn about Next.js in an interactive course with quizzes!
+        </>
+      )
+    },
+    {
+      href: "https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+      title: (
+        <>
+        Deploy &rarr;
+        </>
+      ),
+      text: (
+        <>
+        Instantly deploy your Next.js site to a public URL with Vercel.
+        </>
+      )
+    }
+  ]
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
@@ -24,45 +91,11 @@ export default function Home() {
         </p>
 
         <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {
+            linkCards.map((linkCard, i) => (
+              <LinkCard href={LinkCard.href} title={linkCard.title} text={linkCard.text} key={String(i)} />
+            ))
+          }
         </div>
       </main>
 
