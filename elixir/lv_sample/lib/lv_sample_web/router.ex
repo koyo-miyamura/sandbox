9ef1bd051eba6cbec5bc737698d9d6a6bd9e3623
koyo-miyamura <nexus9071@gmail.com>
2021-09-12 20:28:01 +0900

    ここまで https://qiita.com/piacerex/items/3f8ee18c9443d63955bf#%E6%9C%AC%E3%82%B3%E3%83%A9%E3%83%A0%E3%81%AE%E6%A4%9C%E8%A8%BC%E7%92%B0%E5%A2%83%E4%BA%8B%E5%89%8D%E6%A7%8B%E7%AF%89%E3%81%AE%E3%82%B3%E3%83%9E%E3%83%B3%E3%83%89

--- elixir/lv_sample/lib/lv_sample_web/router.ex
+++ elixir/lv_sample/lib/lv_sample_web/router.ex
@@ -18,6 +18,7 @@ defmodule LvSampleWeb.Router do
     pipe_through :browser
 
     live "/", PageLive, :index
+    live "/realtime", QiitaSearchRealtime
   end
 
   # Other scopes may use custom stacks.
