337b05e94bcc6af2d171dc2823860d7842a63f35
koyo-miyamura <nexus9071@gmail.com>
2021-09-13 00:40:41 +0900

    テンプレート分離

--- elixir/lv_sample/lib/lv_sample_web/live/qiita_search_realtime.ex
+++ elixir/lv_sample/lib/lv_sample_web/live/qiita_search_realtime.ex
@@ -1,35 +1,6 @@
 defmodule LvSampleWeb.QiitaSearchRealtime do
   use Phoenix.LiveView
 
-  def render(assigns) do
-    ~L"""
-        <p>
-        <%= if @message do %><%= @message %><% end %>
-        </p>
-
-        <form phx-submit="submit" phx-change="change">
-        <input type="text" name="query" value="<%= @query %>" placeholder="empty" <%= if @loading, do: "readonly" %> />
-        Query: <%= @query %><br>
-        <input type="submit" value="search" onclick="blur()" <%= if @loading, do: "readonly" %> />
-        </form>
-
-        <table>
-        <tr>
-            <th>ID</th>
-            <th>タイトル</th>
-            <th>作成日</th>
-        </tr>
-        <%= for result <- @results do %>
-        <tr>
-            <td><%= result[ "id" ] %></td>
-            <td><%= result[ "title" ] %></td>
-            <td><%= result[ "created_at" ] %></td>
-        </tr>
-        <% end %>
-        </table>
-    """
-  end
-
   def mount(_params, _session, socket) do
     {:ok, assign(socket, query: "", message: "[Init]", loading: false, results: [])}
   end
