16b2f098e9d322dd8124643e29a41bf035b32798
koyo-miyamura <nexus9071@gmail.com>
2022-08-15 00:53:20 +0900

    mix phx.gen.live Users User users name:string

--- elixir/scroll_sample/lib/scroll_sample_web.ex
+++ elixir/scroll_sample/lib/scroll_sample_web.ex
@@ -91,6 +91,7 @@ defmodule ScrollSampleWeb do
 
       # Import LiveView and .heex helpers (live_render, live_patch, <.form>, etc)
       import Phoenix.LiveView.Helpers
+      import ScrollSampleWeb.LiveHelpers
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
