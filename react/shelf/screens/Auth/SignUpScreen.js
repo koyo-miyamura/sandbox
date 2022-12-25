41344da2ebdcb1fb2d301ee7cd7839192857b6f9
koyo-miyamura <nexus9071@gmail.com>
2022-12-26 01:46:14 +0900

    SignUp and SettingScreen

--- react/shelf/screens/Auth/SignUpScreen.js
+++ react/shelf/screens/Auth/SignUpScreen.js
@@ -1,10 +1,43 @@
-import React from "react";
-import { Box, Text } from "native-base";
+import React, { useState, useContext } from "react";
+import { Box, Button, Input, Text, VStack } from "native-base";
+
+import { Context as AuthContext } from "../../context/AuthContext";
 
 const SignUpScreen = () => {
+  const { signup } = useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <Box flex={1} alignItems="center" justifyContent="center">
-      <Text>SignUpScreen</Text>
+    <Box flex={1} alignItems="center" mt="40">
+      <Text w="80%" textAlign={"start"} fontSize="2xl">
+        SignUp
+      </Text>
+      <VStack w="80%" mt="8">
+        <Input
+          keyboardType="email-address"
+          autoCapitalize="none"
+          size="2xl"
+          my="3"
+          placeholder="email"
+          w="100%"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          autoCapitalize="none"
+          type="password"
+          size="2xl"
+          my="3"
+          placeholder="password"
+          w="100%"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <Button my="3" w="100%" onPress={() => signup(email, password)}>
+          Submit
+        </Button>
+      </VStack>
     </Box>
   );
 };
--- react/shelf/screens/Main/SettingScreen.js
+++ react/shelf/screens/Main/SettingScreen.js
@@ -1,10 +1,22 @@
-import React from "react";
-import { Box, Text } from "native-base";
+import React, { useContext } from "react";
+import { Box, Button, Text } from "native-base";
+
+import { Context as AuthContext } from "../../context/AuthContext";
 
 const SettingScreen = () => {
+  const { signout } = useContext(AuthContext);
   return (
     <Box flex={1} alignItems="center" justifyContent="center">
-      <Text>SettingScreen</Text>
+      <Text fontSize="2xl">SettingScreen</Text>
+      <Button
+        width="80%"
+        my="3"
+        size="lg"
+        colorScheme="danger"
+        onPress={() => signout()}
+      >
+        Logout
+      </Button>
     </Box>
   );
 };
