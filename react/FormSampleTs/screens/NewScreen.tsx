4d159ba0faa28b62ff2872de1a1d492ae5736230
koyo-miyamura <nexus9071@gmail.com>
2022-12-29 23:30:03 +0900

    FormControl で囲んだ

--- react/FormSampleTs/screens/NewScreen.tsx
+++ react/FormSampleTs/screens/NewScreen.tsx
@@ -30,17 +30,23 @@ const NewScreen : React.FC<Props> = ({navigation}) => {
             placeholder="text"
           />
         </FormControl>
-        <Select selectedValue={language} minWidth={200} accessibilityLabel="Select your favorite programming language" placeholder="Select your favorite programming language" onValueChange={itemValue => setLanguage(itemValue)} _selectedItem={{
-          bg: "cyan.600",
-          endIcon: <CheckIcon size={4} />
-        }}>
-          <Select.Item label="JavaScript" value="js" />
-          <Select.Item label="TypeScript" value="ts" />
-          <Select.Item label="C" value="c" />
-          <Select.Item label="Python" value="py" />
-          <Select.Item label="Java" value="java" />
-        </Select>
-        <TextArea h={20} placeholder="Text Area" autoCompleteType={false} />
+        <FormControl isRequired>
+        <FormControl.Label>Select Language</FormControl.Label>
+          <Select selectedValue={language} minWidth={200} accessibilityLabel="Select your favorite programming language" placeholder="Select your favorite programming language" onValueChange={itemValue => setLanguage(itemValue)} _selectedItem={{
+            bg: "cyan.600",
+            endIcon: <CheckIcon size={4} />
+          }}>
+            <Select.Item label="JavaScript" value="js" />
+            <Select.Item label="TypeScript" value="ts" />
+            <Select.Item label="C" value="c" />
+            <Select.Item label="Python" value="py" />
+            <Select.Item label="Java" value="java" />
+          </Select>
+        </FormControl>
+        <FormControl isRequired>
+          <FormControl.Label>自由記述</FormControl.Label>
+          <TextArea h={20} placeholder="Text Area" autoCompleteType={false} />
+        </FormControl>
         <Button onPress={() => handleSubmit()}>
           Submit
         </Button>
