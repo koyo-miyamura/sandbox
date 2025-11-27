1eff323061431608864673032ec54d2fe41ddaf6
koyo-miyamura <nexus9071@gmail.com>
2025-11-28 01:45:16 +0900

    リファクタ

--- go_react_template/backend/internal/handler/converter.go
+++ go_react_template/backend/internal/handler/converter.go
@@ -6,16 +6,12 @@ import (
 	"github.com/samber/lo"
 )
 
-func userFromDomain(du domain.User) User {
-	return User{
-		Id:    du.ID,
-		Name:  du.Name,
-		Email: du.Email,
-	}
-}
-
 func usersFromDomain(domainUsers []domain.User) []User {
 	return lo.Map(domainUsers, func(du domain.User, _ int) User {
-		return userFromDomain(du)
+		return User{
+			Id:    du.ID,
+			Name:  du.Name,
+			Email: du.Email,
+		}
 	})
 }
